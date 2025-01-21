595e0c078a4ade1fddedd5ff7a52f3c16aeb3704
ANJUM FERDOUS <anjumferdous110@gmail.com>
2025-01-21 20:13:00 +0600

    add function

--- index.js
+++ index.js
@@ -5,29 +5,40 @@ const port = 4000;
 const hostName = '127.0.0.1';
 
 const server = http.createServer((req, res) => {
+
+  const handleReadFile = (statusCode, fileLocation) => {
+    fs.readFile(fileLocation, (err, data) => {
+      res.writeHead(statusCode, { "Content-type": "text/html" });
+      res.write(data);
+      res.end();
+    });  
+  
+  }
+
   if (req.url === "/") {
-    fs.readFile("index.html", (err, data) => {
-      res.writeHead(200, { "Content-type": "text/html" });
-      res.write(data);
-      res.end();
-    });
+    handleReadFile(200, "index.html");
   } else if (req.url === "/about") {
-    fs.readFile("about.html", (err, data) => {
-      res.writeHead(200, { "Content-type": "text/html" });
-      res.write(data);
-      res.end();
-    });
+     handleReadFile(200, "about.html");
+    // fs.readFile("about.html", (err, data) => {
+    //   res.writeHead(200, { "Content-type": "text/html" });
+    //   res.write(data);
+    //   res.end();
+    // });
   } else if (req.url === "/contact") {
-    fs.readFile("contact.html", (err, data) => {
-      res.writeHead(200, { "Content-type": "text/html" });
-      res.write(data);
-      res.end();
-    });
+    handleReadFile(200, "contact.html");
+    
+    // fs.readFile("contact.html", (err, data) => {
+    //   res.writeHead(200, { "Content-type": "text/html" });
+    //   res.write(data);
+    //   res.end();
+    // });
   } else 
     fs.readFile("error.html", (err, data) => {
-      res.writeHead(404, { "Content-type": "text/html" });
-      res.write(data);
-      res.end();
+      handleReadFile(404, "error.html");
+      
+      // res.writeHead(404, { "Content-type": "text/html" });
+      // res.write(data);
+      // res.end();
     });
   
 });
